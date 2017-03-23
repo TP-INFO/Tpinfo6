6210ecb6661aa1f8b5448282fcdad9d6a7738208
Mickael Saché <mickael.sache@gmail.com>
2017-03-23 09:23:31 +0100

    Ajouts Divers classes

---
+++ Tpinfo6/Soleil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tpinfo6
+{
+    public class Soleil : Astre
+    {
+
+        /*──────────────────|
+        |      Méthodes     |
+        |──────────────────*/
+
+        public override void SeLever()
+        {
+            Console.WriteLine("Le jour se lève ..\n" +
+                   "Et le soleil illumine la ferme.\n");
+            nuit = false;
+        }
+
+        public override void SeCoucher() 
+        {
+            Console.WriteLine("Le jour se couche ..\n");
+            nuit = true;
+        }
+
+        public Soleil()
+        {
+
+        }
+
+    }//End of Class
+}
