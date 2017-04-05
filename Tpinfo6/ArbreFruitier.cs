09c6cc2377040cce2635bf7ac7cfbcad3b90083c
Clément Flandin <clemfla@live.fr>
2017-04-05 13:42:51 +0200

    ajout ArbreFruitier

---
+++ Tpinfo6/ArbreFruitier.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Tpinfo6
+{
+    public abstract class ArbreFruitier : Végétal
+    {
+        private double _nNbreFruits;
+        
+        public double NNbreFruits
+        {
+            get
+            {
+                return this._nNbreFruits;
+            }
+            set
+            {
+                this._nNbreFruits=value;
+            }
+        }
+
+        public ArbreFruitier()
+        {
+
+        }
+
+        public virtual void PousseNouveauxFruits(double tauxCroissance)
+        {
+            double temp = _nNbreFruits;
+            _nNbreFruits *= tauxCroissance;
+            Console.WriteLine("Aujourd'hui, {0} nouveaux fruits!", _nNbreFruits-temp);
+        }
+    }
+}
